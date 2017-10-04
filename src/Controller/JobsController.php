42d35045ba7c915092d73cf4e2e4b79fa1e1442c
Afnan Nazir <afnannazir.qc@gmail.com>
2017-10-04 12:17:07 +0200

    Job adds crud

---
+++ src/Controller/JobsController.php
@@ -0,0 +1,116 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Jobs Controller
+ *
+ * @property \App\Model\Table\JobsTable $Jobs
+ *
+ * @method \App\Model\Entity\Job[] paginate($object = null, array $settings = [])
+ */
+class JobsController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'contain' => ['Users']
+        ];
+        $jobs = $this->paginate($this->Jobs);
+
+        $this->set(compact('jobs'));
+        $this->set('_serialize', ['jobs']);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Job id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $job = $this->Jobs->get($id, [
+            'contain' => ['Users']
+        ]);
+
+        $this->set('job', $job);
+        $this->set('_serialize', ['job']);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $job = $this->Jobs->newEntity();
+        if ($this->request->is('post')) {
+            $job = $this->Jobs->patchEntity($job, $this->request->getData());
+            if ($this->Jobs->save($job)) {
+                $this->Flash->success(__('The job has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The job could not be saved. Please, try again.'));
+        }
+        $users = $this->Jobs->Users->find('list', ['limit' => 200]);
+        $this->set(compact('job', 'users'));
+        $this->set('_serialize', ['job']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Job id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $job = $this->Jobs->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $job = $this->Jobs->patchEntity($job, $this->request->getData());
+            if ($this->Jobs->save($job)) {
+                $this->Flash->success(__('The job has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The job could not be saved. Please, try again.'));
+        }
+        $users = $this->Jobs->Users->find('list', ['limit' => 200]);
+        $this->set(compact('job', 'users'));
+        $this->set('_serialize', ['job']);
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Job id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $job = $this->Jobs->get($id);
+        if ($this->Jobs->delete($job)) {
+            $this->Flash->success(__('The job has been deleted.'));
+        } else {
+            $this->Flash->error(__('The job could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
