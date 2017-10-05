a4e313181c196ad7e7c4406036089e8816916285
Afnan Nazir <afnannazir.qc@gmail.com>
2017-10-05 15:48:19 +0200

    Database credentials added

--- config/app.php
+++ config/app.php
@@ -222,16 +222,16 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => getenv('DB_HOST'),
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'root',
-            'password' => 'afnan',
-            'database' => 'jobborse',
+            'username' => getenv('DB_USER'),
+            'password' => getenv('DB_PASSWORD'),
+            'database' => getenv('DB_NAME'),
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
